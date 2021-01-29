81fb49cdbe0b23cfac6de194f1db9e9aeead20fe
SimonBiggs <me@simonbiggs.net>
2021-01-30 03:24:58 +1100

    explain oneself

--- get-pymedphys.py
+++ get-pymedphys.py
@@ -7,9 +7,10 @@ app = typer.Typer()
 
 @app.command()
 def main():
+    print("Add in a dummy spinner because this installer isn't doing anything yet...")
     with click_spinner.spinner():
-        time.sleep(1)
-    
+        time.sleep(5)
+
 
 if __name__ == "__main__":
     app()
